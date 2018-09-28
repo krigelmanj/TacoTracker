07a0d67d6b3d4b02431f23d979a678ec9aadf4c7
Joshua Krigelman <krigelmanj@gmail.com>
2018-09-28 05:57:40 -0500

    Added Yelp dependencies.

--- src/main/java/com/example/Farmer/s/Market/controllers/FarmerController.java
+++ src/main/java/com/example/Farmer/s/Market/controllers/FarmerController.java
@@ -1,9 +1,12 @@
 package com.example.Farmer.s.Market.controllers;
 
+import com.example.Farmer.s.Market.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class FarmerController {
 
     @RequestMapping(value="")
-    public String index(Model model){
+    public String index(Model model, HttpServletRequest request, User user){
+        String username = request.getParameter("username");
         model.addAttribute("title", "Farmer's Market");
+        model.addAttribute("username", username);
 
 
         return "market/index";
