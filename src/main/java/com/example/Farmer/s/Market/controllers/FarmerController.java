f6fe4c3283a10a9798b931c09832d8428555f2a8
Joshua Krigelman <krigelmanj@gmail.com>
2018-09-29 11:13:18 -0500

    Created composite build with depreciated YelpAPI.
    Refactored API to comply with New yelp standards for authentication.

--- src/main/java/com/example/Farmer/s/Market/controllers/FarmerController.java
+++ src/main/java/com/example/Farmer/s/Market/controllers/FarmerController.java
@@ -5,6 +5,8 @@ import com.example.Farmer.s.Market.models.User;
 
 import com.montealegreluis.yelpv3.Yelp;
 import com.montealegreluis.yelpv3.businesses.Coordinates;
+import com.montealegreluis.yelpv3.businesses.SearchResult;
+import com.montealegreluis.yelpv3.client.Credentials;
 import com.montealegreluis.yelpv3.search.SearchCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,18 +25,18 @@ public class FarmerController {
         String username = request.getParameter("username");
         model.addAttribute("title", "Farmer's Market");
         model .addAttribute("username", username);
-        com.montealegreluis.yelpv3.client.Credentials clientId = new com.montealegreluis.yelpv3.client.Credentials("UsVIhi8B5OqaSawl-d_MIg","GJM3ct05cXXX0WjMcRN1LLxz5kOJAHs0pTD6Eu0Yu3yChpUFijugH_OF7WFzbxeF09TKcbYvRuYDNU9PCmoBcF2ohwG-c9sqTgZ0_jGGCJP4KENg6Tl8TbtnPQiuW3Yx");
+        Credentials clientId = new Credentials("UsVIhi8B5OqaSawl-d_MIg","GJM3ct05cXXX0WjMcRN1LLxz5kOJAHs0pTD6Eu0Yu3yChpUFijugH_OF7WFzbxeF09TKcbYvRuYDNU9PCmoBcF2ohwG-c9sqTgZ0_jGGCJP4KENg6Tl8TbtnPQiuW3Yx");
 /*
         YelpClient apiKey = new YelpClient("GJM3ct05cXXX0WjMcRN1LLxz5kOJAHs0pTD6Eu0Yu3yChpUFijugH_OF7WFzbxeF09TKcbYvRuYDNU9PCmoBcF2ohwG-c9sqTgZ0_jGGCJP4KENg6Tl8TbtnPQiuW3Yx");
 */
 
         Yelp yelp = new Yelp(clientId);
-        yelp.search(com.montealegreluis.yelpv3.search.SearchCriteria.byLocation("San Antonio"));
-        yelp.search(com.montealegreluis.yelpv3.search.SearchCriteria.byCoordinates(29.426786, -98.489576));
+        yelp.search(SearchCriteria.byLocation("San Antonio"));
+        yelp.search(SearchCriteria.byCoordinates(29.426786, -98.489576));
         yelp.search(SearchCriteria.byCoordinates(new Coordinates(29.426786, -98.489576)));
         SearchCriteria criteria = SearchCriteria.byLocation("San Antonio");
-        com.montealegreluis.yelpv3.businesses.SearchResult result = yelp.search(criteria).searchResult();
-        model.addAttribute("businesses", result.total );
+        SearchResult result = yelp.search(criteria).searchResult();
+        model.addAttribute("businesses", result.businesses );
 
 
         return "market/index";
